24b2f6e9d0e48a61280834c9c209eba9b0d6e151
ahapuWang <55306019+ahapu@users.noreply.github.com>
2025-02-02 05:37:21 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -30,4 +30,4 @@ rm -rf package/lean/luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 rm -rf package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -f $GITHUB_WORKSPACE/asset/bg1.jpg package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/assets/bg1.jpg package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
