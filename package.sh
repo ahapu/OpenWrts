684617e69ef45460d8143dd43f3d0eb974aee64f
ahapuWang <55306019+ahapu@users.noreply.github.com>
2025-02-02 05:44:55 +0000

    Update package.sh

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/O
 
 # Theme
 # luci-theme-neobird
-git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
+# git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 
 # Add luci-app-poweroff
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
