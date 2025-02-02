7bd7df10702725d6384aa80f5cf06ec1248c278d
ahapuWang <55306019+ahapu@users.noreply.github.com>
2025-02-02 19:35:41 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -12,7 +12,7 @@ sed -i 's/255.255.255.0/255.255.255.192/g' package/base-files/files/bin/config_g
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # luci-theme-infinityfreedom
-echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
+# echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
 # passwall
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
@@ -22,12 +22,3 @@ echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
 # echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
 
 # 替换默认主题
-rm -rf package/lean/luci-theme-argon 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
-
-# Add luci-theme
-rm -rf package/lean/luci-theme-argon 
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-rm -rf package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -f $GITHUB_WORKSPACE/assets/bg1.jpg package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
