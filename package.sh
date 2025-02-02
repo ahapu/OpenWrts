875c9fa9a47fda25bc38fd5c7855a7d8c11325e4
ahapuWang <55306019+ahapu@users.noreply.github.com>
2025-02-02 20:37:17 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -22,5 +22,5 @@ rm -rf package/lean/luci-theme-argon-mod
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 rm -rf package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -f ./assets/bg1.jpg package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/assets/bg1.jpg package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 git clone --depth=1 https://github.com/DHDAXCW/theme package/otherapp/theme
